e3baddddaaccf73347eddcb6f89751f6f1cb8ac8
Arad Reed <aradreed@gmail.com>
2015-03-13 01:06:04 -0700

    Added times

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Amiibo-Tracker v.2
+# Amiibo-Tracker v.3
 
 ## Description
-First, I believe this script only work on a Mac for the moment only due to the audio line. Without audio, it should work on either. 
+First, I believe this script only works on a Mac for the moment only due to the audio line. Without audio, it should work on either. 
 
 This script scrapes http://www.nowinstock.net/videogaming/games/amiibo/ for an amiibo alert. The site is scraped every 30 seconds by default and shows the current tracker history. It will play the alert sound when the newest thing added on the tracker history is in stock. (This script is currently untested for an amiibo coming in stock)
 
--- amiiboTrack.rb
+++ amiiboTrack.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-# Amiibo Track v.2
+# Amiibo Track v.3
 # Arad Reed
 # Tracks in stock alerts from nowinstock.net
 # TODO: Allow audio to be played on both Windows and OSX
@@ -9,25 +9,39 @@ require 'mechanize'
 require 'curses'
 
 def get_stock(mech, link)
-  # Grab the most recent data
+  # Grab the most recent stock data
   page = mech.get(link)
   
   data = '//div[@id="data"]/table//tr/td[2]'
   return page.search(data).to_s.gsub("<td>", "").split("</td>")
 end
 
-def output(stock_arr)
+def get_times(mech, link)
+  # Grab the corresponding times
+  page = mech.get(link)
+  
+  data = '//div[@id="data"]/table//tr/td[1]'
+  return page.search(data).to_s.gsub("<td>", "").split("</td>")
+end
+
+def output(stock_arr, times)
   Curses.clear
-  Curses.setpos(0, 0)
   
   # Add all stock to the screen
-  stock_arr.each {|item| Curses.addstr "#{item}\n"}
+  stock_arr.each_with_index do |item, i|
+    Curses.setpos(i, 0)
+    Curses.addstr "#{item}\n"
+    Curses.setpos(i, 50)
+    Curses.addstr "#{times[i].strip}\n"
+    
+  end
+  
   Curses.addstr "Scanning for data.."
   Curses.refresh
 end
 
 # Start Curses screen
-Curses.init_screen
+#Curses.init_screen
 
 # Audio alert to play 
 file = './alert.wav'
@@ -39,18 +53,21 @@ mech = Mechanize.new
 page = mech.get(link)
 stock = get_stock(mech, link)
 
-output(stock)
+times = get_times(mech, link)
+output(stock, times)
 
 # Continously scrape the page for in stock data
 while true
   if (get_stock(mech, link) != stock)
     # Set new stock
     stock = get_stock(mech, link)
+    times = get_times(mech, link)
+    
+    output(stock, times)
     
     # Audio alert
     if (stock[0].include "In Stock")
       pid = fork{ exec 'afplay', file }
-      output(stock)
     end
   end
   
